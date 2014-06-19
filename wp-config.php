97173df3fcec29a9f9d2437a8d3606b88b3c86b7
lheath <lheath@nfusion.com>
2014-06-18 23:45:30 -0500

    Locations widget updating

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'lam_wp');
 
 /** MySQL database username */
-define('DB_USER', 'LAM-14');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Bagu3tt314');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
