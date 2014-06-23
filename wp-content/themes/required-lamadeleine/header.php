6bd2c0c60cdbb0fe47ee47431f8b6838df6f0e74
lheath <lheath@nfusion.com>
2014-06-23 14:44:58 -0500

    Updating icons, QA prep

---
+++ wp-content/themes/required-lamadeleine/header.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div class="container">
+ *
+ * @package required+ Starter
+ * @since required+ Starter 0.1.0
+ */
+?><!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="shortcut icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/images/devices/apple-touch-icon-iphone.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/images/devices/apple-touch-icon-iphone.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/images/devices/apple-touch-icon-ipad.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/images/devices/apple-touch-icon-ipad.png" />
+	
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+<?php
+	wp_head();
+?>
+</head>
+<body <?php body_class(); ?>>
+
+	<?php include_once ('nav.php'); ?>
+
+	<header id="header" role="banner">
+		<div class="header-wrapper">
+			<div class="icon-menu"><span>More</span></div>
+			<div class="logo-wrapper">
+				<a href="<?php echo get_site_url();?>">
+					<img class="logo" src="<?php echo get_stylesheet_directory_uri();?>/img/header/logo.png">
+				</a>
+				<img class="tagline" src="<?php echo get_stylesheet_directory_uri();?>/img/header/tagline.png">
+			</div>
+			<div class="icon-wrapper">
+				<div class="icon icon-phone lam-call"><a href="#"></a></div>
+				<div class="icon icon-pin lam-geolocate"></div>
+				<div class="loading">
+          <div id="floatingCirclesG">
+          <div class="f_circleG" id="frotateG_01">
+          </div>
+          <div class="f_circleG" id="frotateG_02">
+          </div>
+          <div class="f_circleG" id="frotateG_03">
+          </div>
+          <div class="f_circleG" id="frotateG_04">
+          </div>
+          <div class="f_circleG" id="frotateG_05">
+          </div>
+          <div class="f_circleG" id="frotateG_06">
+          </div>
+          <div class="f_circleG" id="frotateG_07">
+          </div>
+          <div class="f_circleG" id="frotateG_08">
+          </div>
+          </div>
+      </div>
+			</div>
+		</div>
+	</header>
+	
+	<div id="container" class="container" role="document">
