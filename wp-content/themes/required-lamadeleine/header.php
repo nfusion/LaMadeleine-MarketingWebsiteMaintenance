36d8e74feea0bdc060c25c13196a1a0913827159
lheath <lheath@nfusion.com>
2014-06-24 00:29:43 -0500

    Set mobile max scale to 1.0

--- wp-content/themes/required-lamadeleine/header.php
+++ wp-content/themes/required-lamadeleine/header.php
@@ -16,7 +16,7 @@
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, max-scale=1.0" />
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="shortcut icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
