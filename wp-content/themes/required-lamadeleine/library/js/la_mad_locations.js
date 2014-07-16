5669f71c0c1dec9e2fa28e06509c3a05a4518e2c
Christian Serna <usernam=christian@christianserna.com>
2014-07-15 22:44:00 -0500

    adding a 3rd oaram time out

--- wp-content/themes/required-lamadeleine/library/js/la_mad_locations.js
+++ wp-content/themes/required-lamadeleine/library/js/la_mad_locations.js
@@ -284,12 +284,12 @@ var LaMadLocations = {
                     // Add temporary .loading-directions class for loading indicator purposes
                     $directionsLink.addClass('loading-directions');
 
-                    navigator.geolocation.getCurrentPosition(this.geoLinkDir, this.geoErr);
+                    navigator.geolocation.getCurrentPosition(this.geoLinkDir, this.geoErr, {timeout:10000});
                 }else if(navigator.geolocation){
                     if(typeof(objOnly) != 'undefined'){
-                        navigator.geolocation.getCurrentPosition(this.geoFoundObjOnly, this.geoErr);
+                        navigator.geolocation.getCurrentPosition(this.geoFoundObjOnly, this.geoErr, {timeout:10000});
                     } else {
-                        navigator.geolocation.getCurrentPosition(this.geoFound, this.geoErr);
+                        navigator.geolocation.getCurrentPosition(this.geoFound, this.geoErr, {timeout:10000});
                     }
                 }
                 else {
