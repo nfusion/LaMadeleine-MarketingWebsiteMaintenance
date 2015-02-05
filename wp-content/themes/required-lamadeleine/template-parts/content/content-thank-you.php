8c8abdbea855109437e820ab0568ea5d8bf1db1b
Roger Frace <rfrace@nfusion.com>
2015-02-05 15:24:06 -0600

    wordpress, plugins updates

---
+++ wp-content/themes/required-lamadeleine/template-parts/content/content-thank-you.php
@@ -0,0 +1,5 @@
+ <div class='store-text row'>
+                <h1>Merci Beaucoup</h1>
+                
+                <p class='subhead'>Your online order has been received and is currently being proccessed.</p>
+            </div>
