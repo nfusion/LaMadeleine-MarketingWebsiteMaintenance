84f8ee53837eed17e0136d7e9aa40adc8acb7429
Michael Davis <mdavis@nfusion.com>
2015-02-06 17:59:56 -0600

    catering theme begins

---
+++ wp-content/themes/lamadeleine-catering/header.php
@@ -0,0 +1,155 @@
+<?php
+// rebuild cache file if it hasn't been done today
+if ( !file_exists('locations.cache') || filemtime('locations.cache') < strtotime( date("Y-m-d") ) ) {
+	if (substr($_SERVER['HTTP_HOST'],0,5) == "local") {
+		$con=mysqli_connect("chiang.office.nfusion","LAM-main","Chees3Cr0issant","lam_main_legacy");
+	} else {
+		$con=mysqli_connect("localhost","LAM-main","Chees3Cr0issant","lam_main_legacy");
+	}
+	
+	$result = mysqli_query($con,"SELECT * FROM wp_store_locator WHERE sl_catering = 1 ORDER BY sl_state ASC, sl_city ASC");
+
+	$state_list = array('AL' => 'Alabama', 'AK' => 'Alaska', 'AZ' => 'Arizona', 'AR' => 'Arkansas', 'CA' => 'California', 'CO' => 'Colorado', 'CT' => 'Connecticut', 'DE' => 'Delaware', 'FL' => 'Florida', 'GA' => 'Georgia', 'HI' => 'Hawaii', 'ID' => 'Idaho', 'IL' => 'Illinois', 'IN' => 'Indiana', 'IA' => 'Iowa', 'KS' => 'Kansas', 'KY' => 'Kentucky', 'LA' => 'Louisiana', 'ME' => 'Maine', 'MD' => 'Maryland', 'MA' => 'Massachusetts', 'MI' => 'Michigan', 'MN' => 'Minnesota', 'MS' => 'Mississippi', 'MO' => 'Missouri', 'MT' => 'Montana', 'NE' => 'Nebraska', 'NV' => 'Nevada', 'NH' => 'New Hampshire', 'NJ' => 'New Jersey', 'NM' => 'New Mexico', 'NY' => 'New York', 'NC' => 'North Carolina', 'ND' => 'North Dakota', 'OH' => 'Ohio', 'OK' => 'Oklahoma', 'OR' => 'Oregon', 'PA' => 'Pennsylvania', 'RI' => 'Rhode Island', 'SC' => 'South Carolina', 'SD' => 'South Dakota', 'TN' => 'Tennessee', 'TX' => 'Texas', 'UT' => 'Utah', 'VT' => 'Vermont', 'VA' => 'Virginia', 'WA' => 'Washington', 'WV' => 'West Virginia', 'WI' => 'Wisconsin', 'WY' => 'Wyoming', 'DC' => 'District of Columbia');
+	$current_state = '';
+	$location_links = '';
+	$location_items = '';
+	$states_menu = '';
+	$count = 0;
+	$all_locations = array();
+
+	while($row = mysqli_fetch_array($result)) {
+		if(!$row['sl_region']) $row['sl_region'] = $row['sl_city'];
+
+		$all_locations[strtoupper($row['sl_state'])][$row['sl_region']][] = $row;
+	}
+
+	mysqli_close($con);
+
+	foreach ($all_locations as $state => $regions) {
+		$states_menu .= '<li><a href="/#' . $state . '">' . $state_list[$state] . '</a></li>';
+		$location_links .= ($location_links == '') ? '<a href="#' . $state . '">' . $state_list[$state] . '</a>' : '&emsp;|&emsp;<a href="#' . str_replace(" ", "_", $state) . '">' . $state_list[$state] . '</a>';
+		
+		$location_items .= '<div id="' . $state . '" class="state_group">' . "\n" .
+			'<h3>' . $state_list[$state] . '</h3>' . "\n";
+
+		foreach ($regions as $region => $region_locations) {
+			$count = 0;
+
+			$location_items .= '<div class="region_group">' . "\n" .
+				'<h4>' . $region . '</h4>' . "\n";
+
+			foreach ($region_locations as $location) {
+				$location_items .= '<div class="location_item item' . ($count + 1) . '">' . "\n" .
+					'<span class="location_name">' . $location['sl_store'] . '</span>' . "\n" .
+					'<span class="location_street">' . $location['sl_address'] . '</span>' . "\n" .
+					'<span class="location_street2">' . $location['sl_address2'] . '</span>' . "\n" .
+					'<span class="location_citystatezip">' . $location['sl_city'] . ', ' . strtoupper($location['sl_state']) . ' ' . $location['sl_zip'] . '</span>' . "\n" .
+					'<span class="location_phone">' . $location['sl_phone'] . '</span>' . "\n" .
+					'<span class="location_menu"><a href="http://order.cateringbylamadeleine.com/" title="Click to Order" target="_blank">Click to Order</a></span>' . "\n" .
+					'</div>' . "\n";
+
+				$count = 1 - $count;
+			}
+
+			$location_items .= '</div>' . "\n";
+		}
+
+		$location_items .= '</div>' . "\n";
+	}
+
+	$locations = '<div id="jump_links">' . "\n" . $location_links . "\n" . '</div>' . $location_items;
+
+	$fp = fopen('locations.cache', 'w');
+	fwrite($fp, $locations);
+	fclose($fp);
+
+	$fp = fopen('states.cache', 'w');
+	fwrite($fp, $states_menu);
+	fclose($fp);
+}
+else {
+	// Cache has been created for today
+	$locations = file_get_contents('locations.cache');
+	$states_menu = file_get_contents('states.cache');
+}
+?><!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie10 lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie10 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js lt-ie10" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+
+	<title>Catering by La Madeleine</title>
+
+	<meta name="author" content="" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+	<link rel="shortcut icon" href="/img/favicon.ico">
+	<link rel="apple-touch-icon" sizes="57x57" href="/img/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/img/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/img/apple-touch-icon-114x114.png">
+
+	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/libs/modernizr-2.5.3.min.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Lobster' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+	
+	<!--jQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
+
+	<?php wp_head(); ?> 
+</head>
+
+<body <?php body_class( $class ); ?>> 
+	<header id="header" class="navbar" role="banner">
+		<div class="container">
+			<div class="brand"><a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Catering by La Madeleine" /></a></div>
+			<div id="mobile_nav">
+				<div id="mobile_open"></div>
+				<div id="mobile_close">Close</div>
+			</div>
+			<nav id="nav_main" role="navigation">
+				<div class="left_nav">
+					<ul>
+						<li class="nav_catering">
+							<a href="/menu">CATERING MENU</a>
+							<ul class="submenu">
+								<li><a href="/menu#breakfast">Breakfast</a></li>
+								<li><a href="/menu#sandwiches">Sandwiches</a></li>
+								<li><a href="/menu#boxes">bistro box lunches</a></li>
+								<li><a href="/menu#salades">salades</a></li>
+								<li><a href="/menu#entrees">entrées</a></li>
+								<li><a href="/menu#pastas">pastas</a></li>
+								<li><a href="/menu#soupes">soupes</a></li>
+								<li><a href="/menu#a-la-carte">à la carte</a></li>
+								<li><a href="/menu#beverages">beverages</a></li>
+								<li><a href="/menu#pastries-desserts">pastries & desserts</a></li>
+								<li class="nav_download"><a target="_blank" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/LAMAD_8957_4_mch_mnu_catering_ForWeb.pdf">Download Catering Menu</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="http://order.cateringbylamadeleine.com/">ORDER ONLINE</a>
+						</li>
+						<li class="nav_locations">
+							<a href="/#location_list">FIND A CAFé</a>
+							<ul class="submenu"><?php echo $states_menu; ?></ul>
+						</li>
+					</ul>
+				</div>
+				<div class="right_nav">
+					<ul>
+						<li>
+							<a href="/faq">FREQUENTLY ASKED QUESTIONS</a>
+						</li>
+						<li>
+							<a target="_blank" href="http://lamadeleine.com/">LAMADELEINE.COM</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</header>
---
+++ wp-content/themes/lamadeleine-catering/holiday-feast-2014.php
@@ -0,0 +1,103 @@
+<?php require_once('header.php'); ?>
+<?php include_once('js/trackingjs.html'); ?>
+
+<div id="banners">
+	<div class="container">
+		<div id="banner_images">
+			<div id="">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/banners/holiday-feast-nb.jpg" alt="Holiday Feast" />
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="main" role="document" class="holiday-feast">
+	<div class="container">
+		<div id="swirl"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/swirl.png" alt="" /></div>
+		<div id="content">
+			<h1 class="lobster">la Madeleine holiday feasts</h1>
+			<div id="content_left">
+				<h5 class="lobster">There’s a reason every recipe in our holiday feast is crafted with care…</h5>
+				<p>So you can impress every single guest. Discover how <strong>la Madeleine Holiday Feasts</strong> can make your gatherings the simplest, most délicieux you’ve ever hosted.</p>
+				<div class="catering_block">
+						<a href="http://order.cateringbylamadeleine.com/" id="splat"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/orderonline.png" alt="Online Ordering Now Available!" /></a>
+						<div id="splat2">or call 800-96-LAMAD</div>
+				</div>
+				<p class="social_links"><a class="facebook" href="http://www.facebook.com/laMadeleineCafe">&nbsp;</a><a class="twitter" href="http://twitter.com/lamadeleine">&nbsp;</a><a class="pinterest" href="http://pinterest.com/lamadeleinecafe/">&nbsp;</a></p>
+			</div>
+		</div>
+		<div id="menu_content">
+			<div id="menu_nav" class="feast-jump">
+				<h2 class="lobster">order a holiday feast for</h2>
+				<a href="#family-feast" id="jump_links">la family</a>
+				<a href="#office-feast" id="jump_links">le office</a>
+			</div>
+			<div id="menu">
+				<div class="menu_group" id="family-feast">
+					<h2>holiday feast for la family</h2>
+					<p class="subHead">Pick-up, Heat &amp; Serve</p>
+					<div class="menu_item"><p>Fill your table with seasonal goodness. We offer a complete feast for your family, including your choice of two desserts. <br /><span>(Serves 8.) $109.99 | $13.75 per person</span> </p></div>
+
+					<div class="menu_item">
+						<h4>your feast includes</h4>
+						<p>oven-roasted turkey breast • cranberry apple stuffing • potato gratin • green beans almondine* • wild mushroom sauce • cranberry orange chutney • sourdough petit pain &amp; butter</p>
+					</div>
+
+					<div class="menu_item">
+						<h4>plus your choice of 2 desserts</h4>
+						<p>pumpkin pie* • pecan tart* • grand chocolate tart* • grand lemon tart* • bûche de noël†</p>
+					</div>
+					
+					<div class="menu_item margin-top-60">
+						<h3>family feast à la carte selections</h3>
+						<p><small>Each dish serves up to 8. Requires some preparation at home.</small></p>
+						<p>holiday sides package | $59.99 • hickory-smoked spiral-cut ham | $49.99<br />
+						oven-roasted turkey breast | $39.99 • cranberry apple stuffing | $13.99 • potato gratin | $12.99<br />
+						green beans almondine* | $11.99 • wild mushroom sauce | $7.99 <br />
+						cranberry orange chutney | $7.99 • sourdough petit pain | $7.99/dozen
+						</p>
+						<p class="margin-top-30"><i>Family feast for a smaller group of 4 is available.</i></p>
+					</div>
+					<div class="order-wrapper">
+						<a href="https://order.lamadeleine.com/index.cfm?" class="cta uppercase">Click here to order</a>
+						<p>or call <a href="tel:18009652623">1-800-96-lamad</a></p>
+						<p class="margin-top-30 download-link" style="text-align:center;">Download Feast for la Family Re-heating Instructions<br />
+						<a href="assets/2014_Holiday_Reheating_Instructions_-_For_4.pdf">Feast for 4</a> or 
+						<a href="assets/2014_Holiday_Reheating_Instructions_-_For_8.pdf">Feast for 8</a></p>
+					</div>
+				</div>
+				<div class="menu_group margin-top-30" id="office-feast">
+					<h2>holiday feast for le office</h2>
+					<p class="subHead">DELIVERED HOT &amp; SET UP AT YOUR OFFICE EVENT OR PARTY</p>
+					<div class="menu_item">
+						<p>Treat your coworkers to a celebration of flavor. Chafing dishes, serveware, plates and utensils provided.<br /><span>(Serves 20.) $219.99 | $10.99 per person</span></p>
+					</div>
+					<div class="menu_item">
+						<h3>your feast includes</h3>
+						<p>sliced oven-roasted turkey breast • cranberry apple stuffing • potato gratin<br />green beans almondine* • wild mushroom sauce • cranberry orange chutney<br />sourdough petit pain &amp; butter</p>
+						<p class="margin-top-30"><i>Orders must be placed at least 24 hours in advance of desired delivery time.</i></p>
+					</div>
+					
+					<div class="order-wrapper">
+						<a href="https://order.lamadeleine.com/index.cfm?" class="cta uppercase">Click here to order</a>
+						<p>or call <a href="tel:18009652623">1-800-96-lamad</a></p>
+					</div>
+					<div class="menu_item dessert">
+						<img src="img/holidayFeasts_Dessert.jpg" alt="Holiday Feast Dessert"/>
+						<h3>add our signature &amp; delectable holiday pâtisseries for your table</h3>
+						<p>white chocolate raspberry cake* • bûche de noël† • pumpkin pie* • pecan tart*<br />grand lemon tart* • grand chocolate tart* • mini tarts &amp; parfaits*</p>
+						<p>and so much more...</p>
+					</div>
+				</div>
+				<div class="menu_group" id="order-dates">
+					<p><i>Thanksgiving Orders: Please place order by 11/23. Pick up as late as noon on 11/27, Thanksgiving Day.</i></p>
+					<p><i>Christmas Orders: Please place order by 12/21. Pick up as late as 4 p.m. on 12/24, Christmas Eve.</i></p>
+					<p class="margin-top-30">*contains nuts  | † available only after Thanksgiving</p>
+					<a href="#top" class="cta uppercase">Back to top</a>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div><!-- /.container -->
+</div><!-- /#main -->
+<?php require_once('footer.php'); ?>
