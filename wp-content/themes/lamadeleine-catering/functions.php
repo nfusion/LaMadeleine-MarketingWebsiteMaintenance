a2d4441994644b92a6e3d738d90cbe4cf7e4488d
Michael Davis <mdavis@nfusion.com>
2015-02-11 19:51:54 -0600

    added menu items post type

--- wp-content/themes/lamadeleine-catering/functions.php
+++ wp-content/themes/lamadeleine-catering/functions.php
@@ -3,6 +3,7 @@
 * 	Author: nFusion
 */
 
+/*** Header Navigation ***/
 function header_menus() {
   register_nav_menus(
     array(
@@ -12,3 +13,37 @@ function header_menus() {
   );
 }
 add_action( 'init', 'header_menus' );
+
+/*** Custom Taxonomies ***/
+// function add_custom_taxonomies() {
+// 	register_taxonomy('Menu Categories','page', array(
+// 		),)
+// }
+
+/*** Custom Post Types ***/
+function menu_post_type() {
+  $labels = array(
+    'name'               => _x( 'Menu Items', 'post type general name' ),
+    'singular_name'      => _x( 'Menu Item', 'post type singular name' ),
+    'add_new'            => _x( 'Add New', '' ),
+    'add_new_item'       => __( 'Add New Menu Item' ),
+    'edit_item'          => __( 'Edit Menu Item' ),
+    'new_item'           => __( 'New Menu Item' ),
+    'all_items'          => __( 'All Menu Items' ),
+    'view_item'          => __( 'View Menu Item' ),
+    'search_items'       => __( 'Search Menu' ),
+    'not_found'          => __( 'No menu item found' ),
+    'not_found_in_trash' => __( 'No menu item found in the Trash' ), 
+    'parent_item_colon'  => '',
+    'menu_name'          => 'Menu Items'
+  );
+  $args = array(
+    'labels'        => $labels,
+    'public'        => true,
+    'supports'      => array( 'title','editor','custom-fields' ),
+    'has_archive'   => true,
+  );
+  register_post_type( 'menu_items', $args ); 
+  flush_rewrite_rules();
+}
+add_action( 'init', 'menu_post_type' );
