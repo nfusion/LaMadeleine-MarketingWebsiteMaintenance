f38892b324dc3aae086226f3937c9f1fddeee50e
Michael Davis <mdavis@nfusion.com>
2015-02-17 15:23:28 -0600

    title issues

--- wp-content/themes/lamadeleine-catering/functions.php
+++ wp-content/themes/lamadeleine-catering/functions.php
@@ -84,7 +84,7 @@ add_filter( 'wp_title', 'baw_hack_wp_title_for_home' );
 function baw_hack_wp_title_for_home( $title )
 {
   if( empty( $title ) && ( is_home() || is_front_page() ) ) {
-    return __( 'Home', 'theme_domain' ) . ' | ' . get_bloginfo( 'description' );
+    return __( 'catering by la Madeleine', 'theme_domain' ) . ' | ' . get_bloginfo( 'description' );
   }
   return $title;
 }
