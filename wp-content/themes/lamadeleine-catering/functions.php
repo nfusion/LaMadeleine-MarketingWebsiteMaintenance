6a208394e7a4b3a2905c9a64a10c3fdd970cf7c3
Michael Davis <mdavis@nfusion.com>
2015-02-17 15:21:34 -0600

    title issues

--- wp-content/themes/lamadeleine-catering/functions.php
+++ wp-content/themes/lamadeleine-catering/functions.php
@@ -78,3 +78,13 @@ function getCateringLocations() {
   $json = file_get_contents($file);
   return json_decode($json);
 }
+
+//Home Title hack
+add_filter( 'wp_title', 'baw_hack_wp_title_for_home' );
+function baw_hack_wp_title_for_home( $title )
+{
+  if( empty( $title ) && ( is_home() || is_front_page() ) ) {
+    return __( 'Home', 'theme_domain' ) . ' | ' . get_bloginfo( 'description' );
+  }
+  return $title;
+}
