5bf720d81fe653f1d3e0f36d411f101c599a3b73
Michael Davis <mdavis@nfusion.com>
2015-02-17 12:48:18 -0600

    QA fixes. fixed submenu showing at mobile

--- wp-content/themes/lamadeleine-catering/page-menu.php
+++ wp-content/themes/lamadeleine-catering/page-menu.php
@@ -68,7 +68,7 @@ $get_items = new WP_Query( $menu_items );
                         </div>
                         <div class="catering_block menu-footer">
                             <p class="center"><a href="#">Back To Top</a></p>
-                            <p><a target="_blank" href="<?php echo get_stylesheet_directory_uri(); ?>/LAMAD_8957_4_mch_mnu_catering_ForWeb.pdf">DOWNLOAD CATERING MENU</a></p>
+                            <p><a target="_blank" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/LAMAD_8957_4_mch_mnu_catering_ForWeb.pdf">DOWNLOAD CATERING MENU</a></p>
                         </div>
             <?php endwhile; // end of the loop. ?>
             </div><!-- /menu-content -->
