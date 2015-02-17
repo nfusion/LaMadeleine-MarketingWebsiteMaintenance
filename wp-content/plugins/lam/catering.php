2615cdf5b7593f929b34640341c479bc58a7871d
Katherine White <kwhite@nfusion.com>
2015-02-17 16:29:09 -0600

    Adding order by clause for a city-based sort.

--- wp-content/plugins/lam/catering.php
+++ wp-content/plugins/lam/catering.php
@@ -90,7 +90,8 @@ class Catering_Locations {
 			"FROM " . $wpdb->prefix . "posts p " . 
 			"INNER JOIN " . $wpdb->prefix . "pods_locations l " . 
 			"ON p.ID = l.id " . 
-			"WHERE l.catering_available = 1", OBJECT );
+			"WHERE l.catering_available = 1 " . 
+			"ORDER BY l.city ASC", OBJECT );
 		
 		// build an array that is keyed off of the post ID.
 		foreach ($catering as $location) {
