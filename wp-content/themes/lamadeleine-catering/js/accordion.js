75c1aa2219d31b5d922022e8270b158745658cb5
Katherine White <kwhite@nfusion.com>
2015-02-18 13:25:21 -0600

    BugzID: 14438. Tied auto open to window hash

--- wp-content/themes/lamadeleine-catering/js/accordion.js
+++ wp-content/themes/lamadeleine-catering/js/accordion.js
@@ -1,15 +1,11 @@
 jQuery(document).ready(function() {
-
-	if (window.location.href.indexOf('faq') || window.location.href.indexOf('frequently-asked-questions')) {
-		// set up the check to fire once the accordion has rendered.
-		jQuery("#accordion").on("accordioncreate", function(){
-			if (window.location.hash) {
-				var id = window.location.hash.substr(1);
-				openAccordionElement(id);
-			}
-		})
-		
-	}
+	// set up the check to fire once the accordion has rendered.
+	jQuery("#accordion").on("accordioncreate", function(){
+		if (window.location.hash) {
+			var id = window.location.hash.substr(1);
+			openAccordionElement(id);
+		}
+	});
 
 	jQuery("#accordion").accordion({
 		autoHeight: false, 
