69b7bcd28f2f185a93b28f0038e667e8c804f606
Katherine White <kwhite@nfusion.com>
2015-02-18 12:29:28 -0600

    BugzID: 14438. Function to open a specific container.

--- wp-content/themes/lamadeleine-catering/js/accordion.js
+++ wp-content/themes/lamadeleine-catering/js/accordion.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function() {
 	jQuery("#accordion").accordion({autoHeight: false, collapsible: true, active: false, heightStyle: "content"}).css("height","auto");
 });
+
+function openAccordionElement(id) {
+	var header = jQuery('#accordion').find('.ui-accordion-header[aria-controls=ui-id-' + id + ']');
+	//console.log(header);
+  	header.click();
+}
