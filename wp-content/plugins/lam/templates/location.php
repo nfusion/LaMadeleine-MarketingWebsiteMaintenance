d117b3b92ed45e0b02531037230230dcb3380a56
Michael Davis <mdavis@nfusion.com>
2015-04-13 17:13:19 -0500

    reverted FAQ since the creators most recent is broken

---
+++ wp-content/plugins/lam/templates/location.php
@@ -0,0 +1,176 @@
+<?php wp_enqueue_script('myscript', '/wp-content/plugins/lam/js/jQuery.cookie.js'); ?>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"> </script>
+<script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/tags/markerclustererplus/2.0.3/src/markerclusterer.js"></script>
+
+<script type="text/javascript">
+
+    $(document).ready(function(){
+
+        // Do we have a location stored in client cookie?
+        LaMadLocations.getLocationCookie();
+
+        /* Location CTA click handling */
+
+        // Use my location (geolocate)
+        $('#use-location').on('click', function(e){
+            e.preventDefault();
+
+            // Fade out UI elements and hide
+            LaMadLocations.$frontWrapper().fadeOut('fast');
+
+            // Show loading indicator
+            LaMadLocations.$locationCta.addClass('map-loading');
+
+            LaMadLocations.getLocation();
+        });
+
+
+        $('#use-zip').on('click', function(e){
+            e.preventDefault();
+
+            // Fade out UI elements and hide
+            LaMadLocations.$frontWrapper().fadeOut('fast');
+
+            // Show loading indicator
+            LaMadLocations.$locationCta.addClass('map-loading');
+
+            // Use zipcode for location
+            LaMadLocations.geoCodeZip($('#zip-input').val());
+        });
+
+        $('#use-zip-secondary').on('click', function(e){
+            e.preventDefault();
+
+            // Use zipcode for location
+            LaMadLocations.geoCodeZip($('#zip-input-secondary').val());
+        });
+
+        // Submit zip form on enter
+        $('#zip-input').keypress(function(event){
+          if(event.keyCode == 13){
+            $('#use-zip').click();
+          }
+        });
+
+        // Submit zip form on enter
+        $('#zip-input-secondary').keypress(function(event){
+          if(event.keyCode == 13){
+            $('#use-zip-secondary').click();
+          }
+        });
+    });
+
+</script>
+
+<?php 
+    if($onLocationPage == 'false') :
+        $locationClass = "map-view";
+    else:
+        $locationClass = "location-view";
+    endif;
+?>
+
+<div class="widget widget-location <?php echo $locationClass; ?>">
+
+    <div id="widget-location-mobile">
+        
+    </div>
+    
+    <div id="widget-location-full">
+
+        <div class='widget-title'>
+            <h3><?php echo $title ?></h3> <?php if($onLocationPage == 'false'){ echo '<a href="/locations/">See&nbsp;All</a>';}; ?><br>
+        </div>
+
+        <div id="location-cta">
+            <div class="no-locations">
+                <p>D&eacute;sol&eacute;, there are no locations within 100 miles of your search. Please visit our Locations page to view all locations.</p>
+            </div>
+            <div class="flipper">
+                <div class="front">
+                    <div class="front-wrapper">
+                        <div class="geo">
+                            <div class="icon icon-pin"></div>
+                            <p><a id="use-location" class="btn" href="#">Use My Location</a></p>
+                        </div>
+                        <div class="location-cta-divider"></div>
+                        <div class="zip">
+                            <div class="icon icon-magnify"></div>
+                            <input id="zip-input" maxlength="5" placeholder='Enter Zip Code'> <a id="use-zip" class="btn" href="#">Go</a>
+                        </div>
+                    </div>
+                </div>
+
+                <?php
+
+                /** Home view only **/
+
+                // Home view - Small map container
+                $map = '<div id="map"></div>';
+
+                // Home view - Button wrapper and buttons
+                // $btnWrapper = '
+                // <div class="btn-wrapper">
+                //     <a class="btn" target="_blank" href="https://online.lamadeleine.com/">To Go</a>
+                //     <a class="btn" target="_blank" href="http://cateringbylamadeleine.com">Catering</a>
+                // </div>';
+
+                $btnWrapper = '';
+
+                /** Location view only **/
+
+                // Loading indicator
+                $loading = '
+                <div class="loading">
+                    <div class="floatingCirclesG">
+                        <div class="f_circleG frotateG_01"></div>
+                        <div class="f_circleG frotateG_02"></div>
+                        <div class="f_circleG frotateG_03"></div>
+                        <div class="f_circleG frotateG_04"></div>
+                        <div class="f_circleG frotateG_05"></div>
+                        <div class="f_circleG frotateG_06"></div>
+                        <div class="f_circleG frotateG_07"></div>
+                        <div class="f_circleG frotateG_08"></div>
+                    </div>
+                </div>';
+
+                // Locations view - Button wrapper and buttons
+                $btnWrapperLocations = '
+                <div class="btn-wrapper">
+                    <a class="btn-light daypart-menu" href="#">Menu</a>
+                    <a class="btn-light get-directions" href="#"><span class="text">Directions</span>' . $loading . '</a>
+                    <a class="btn-light order-online" target="_blank" href="#"><span class="text">Order</span>'  . $loading . '</a>
+                </div>'; //https://online.lamadeleine.com
+
+                // Locations view - Locations list container
+                $locationsList = '<div id="location-list"></div>';
+
+                /** Both home and location views **/
+
+                // Selected location image container
+                $locationImage = '<div id="location-image"></div>';
+
+                // Selected location info container
+                $locationInfo = '<div id="location-info"></div>';
+
+                // If this is the location view
+                if($onLocationPage == 'true') { 
+                    $locationMarkup = '<div class="back location-view">' . $locationImage . $locationInfo . $btnWrapperLocations . '</div>' . $loading . '<div class="other-locations"><hr class="dashed"><h3>Other Nearby Locations</h3>' . $locationsList . '</div>';
+                } 
+                // Else home view
+                else {
+                    $locationMarkup = '<div class="back map-view"><a class="map-link" href="/locations"></a><div id="returned_map">' . $map . $locationInfo . $btnWrapper . '</div></div>' . $loading;
+                }
+
+                echo $locationMarkup;
+
+                ?>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
---
+++ wp-content/themes/required-lamadeleine/category.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * The template for displaying catagory  archives.
+ *
+ * This is the template that displays catagory  archives by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package required+ Foundation
+ * @since required+ Foundation 0.3.0
+ */
+
+get_header();
+	
+$cat = strtolower(get_cat_name($cat));
+$mypods = pods('post')->find(array('limit' => 0, 'where'=>"category.name='".$cat."'", 'orderby' => "date DESC"));
+$stories = process_stories($mypods);
+
+?>
+
+<div id="content" class="<?php echo $pagename; ?>">
+	<div id="main" role="main">
+		<div class="post-box">
+
+			<div id="mobile-nav">
+			    <a href="/stories/food" <?php if($cat === 'food'){echo 'class="active"';}; ?>>
+			        <div class="nav-item">
+			            <div class="icon icon-food"></div>
+			            Food
+			        </div>
+			    </a>
+			    <a href="/stories/culture" <?php if($cat === 'culture'){echo 'class="active"';}; ?>>
+			        <div class="nav-item">
+			            <div class="icon icon-culture"></div>
+			            Culture
+			        </div>
+			    </a>
+			    <a href="/stories/community" <?php if($cat === 'community'){echo 'class="active"';}; ?>>
+			        <div class="nav-item">
+			            <div class="icon icon-community"></div>
+			            Community
+			        </div>
+			    </a>
+			    <a href="/stories">
+			        <div class="nav-item">
+			            <div class="icon icon-stories"></div>
+			            All Stories
+			        </div>
+			    </a>
+			</div>
+
+<?php
+
+		echo display_story_carousel($stories);
+
+		// foreach($stories as $key => $story){
+		    
+		// }
+
+?>
+
+			</div>
+		</div><!-- /#main -->
+
+		<aside id="sidebar" role="complementary">
+			<div class="sidebar-wrapper">
+				<?php 
+					dynamic_sidebar('sidebar-story');
+
+					// Get the pods content for page ID 201 (stories index)
+					$podPage = pods('page', 201);
+
+					// Get promo relationship field
+					$promo = $podPage->field('fma_promo');
+
+					// Display promo markup
+					echo display_promo($promo, 'widget');
+				?>
+			</div>
+		</aside><!-- /#sidebar -->
+
+	</div><!-- End Content row -->
+
+<?php get_footer(); ?>
