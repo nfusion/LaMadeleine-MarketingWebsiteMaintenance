b75f4ec9c13b6b155bd9889952b388e927b7f635
rfrace <rfrace@nfusion.com>
2015-06-10 12:09:50 -0500

    plugin updates

--- wp-content/plugins/wp-smushit/wp-smush.php
+++ wp-content/plugins/wp-smushit/wp-smush.php
@@ -4,7 +4,7 @@ Plugin Name: WP Smush
 Plugin URI: http://wordpress.org/extend/plugins/wp-smushit/
 Description: Reduce image file sizes, improve performance and boost your SEO using the free <a href="https://premium.wpmudev.org/">WPMU DEV</a> WordPress Smush API.
 Author: WPMU DEV
-Version: 2.0.5
+Version: 2.0.6.1
 Author URI: http://premium.wpmudev.org/
 Textdomain: wp_smush
 */
@@ -36,7 +36,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * Constants
  */
 $prefix          = 'WP_SMUSH_';
-$version         = '2.0.5';
+$version         = '2.0.6.1';
 $smush_constatns = array(
 	'VERSON'            => $version,
 	'BASENAME'          => plugin_basename( __FILE__ ),
@@ -46,7 +46,7 @@ $smush_constatns = array(
 	'DIR'               => plugin_dir_path( __FILE__ ),
 	'URL'               => plugin_dir_url( __FILE__ ),
 	'MAX_BYTES'         => 1000000,
-	'PREMIUM_MAX_BYTES' => 8000000,
+	'PREMIUM_MAX_BYTES' => 32000000,
 	'PREFIX'            => 'wp-smush-',
 	'TIMEOUT'           => 30
 
@@ -695,7 +695,7 @@ if ( ! class_exists( 'WpSmush' ) ) {
 
 				//IF current compression is lossy
 				if ( ! empty( $wp_smush_data ) && ! empty( $wp_smush_data['stats'] ) ) {
-					$lossy    = $wp_smush_data['stats']['lossy'];
+					$lossy    = !empty( $wp_smush_data['stats']['lossy'] ) ? $wp_smush_data['stats']['lossy'] : '';
 					$is_lossy = $lossy == 1 ? true : false;
 				}
 
