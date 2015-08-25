6eca46a6c7c7563976b019297765b396296aad86
Katherine White <kwhite@nfusion.com>
2015-08-25 14:03:39 -0500

    BugzID: 14844. Sizmek installation.

--- wp-content/themes/required-lamadeleine/header.php
+++ wp-content/themes/required-lamadeleine/header.php
@@ -37,7 +37,46 @@
 
 </head>
 <body <?php body_class(); ?>>
+	<!-- sizemek versatag -->
+	<script>
+		var versaTag = {};
+		versaTag.id = "3677";
+		versaTag.sync = 0;
+		versaTag.dispType = "js";
+		versaTag.ptcl = "HTTP";
+		versaTag.bsUrl = "bs.serving-sys.com/BurstingPipe";
 
+		//VersaTag activity parameters include all conversion parameters including custom parameters and Predefined parameters. Syntax: "ParamName1":"ParamValue1", "ParamName2":"ParamValue2". ParamValue can be empty.
+		versaTag.activityParams = {
+			//Predefined parameters:
+			"Session":""
+			//Custom parameters:
+		};
+		//Static retargeting tags parameters. Syntax: "TagID1":"ParamValue1", "TagID2":"ParamValue2". ParamValue can be empty.
+		versaTag.retargetParams = {};
+
+		//Dynamic retargeting tags parameters. Syntax: "TagID1":"ParamValue1", "TagID2":"ParamValue2". ParamValue can be empty.
+		versaTag.dynamicRetargetParams = {};
+
+		// Third party tags conditional parameters and mapping rule parameters. Syntax: "CondParam1":"ParamValue1", "CondParam2":"ParamValue2". ParamValue can be empty.
+		versaTag.conditionalParams = {};
+	</script>
+
+	<script id="ebOneTagUrlId" src="http://ds.serving-sys.com/SemiCachedScripts/ebOneTag.js"></script>
+
+	<noscript>
+		<iframe src="?
+		cn=ot&amp;
+		onetagid=3677&amp;
+		ns=1&amp;
+		activityValues=$$Session=[Session]$$&amp;
+		retargetingValues=$$$$&amp;
+		dynamicRetargetingValues=$$$$&amp;
+		acp=$$$$&amp;"
+		style="display:none;width:0px;height:0px"></iframe>
+	</noscript>
+	<!-- /sizemek -->
+	
 	<?php include_once ('nav.php'); ?>
 
 	<header id="header" role="banner">
